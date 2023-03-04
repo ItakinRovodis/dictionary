8309bb015abe96af29d5ff45bce179f82c4260cd
ItakinRovodis <ItakinRovodis@gmail.com>
2023-03-05 01:05:34 +0300

    changeadd

--- dictionary.c
+++ dictionary.c
@@ -3,7 +3,8 @@
 int main() {
 	HashTable* ht = create_table(CAPACITY);
 	ht_insert(ht, "e", 1);
-	ht_insert(ht, "f", 321);
+	ht_insert(ht, "e", 1);
+	ht_insert(ht, "f", 2);
 	print_search(ht, "e");
     print_search(ht, "f");
     print_search(ht, "3");
